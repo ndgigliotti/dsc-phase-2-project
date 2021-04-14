0d632d7ab94345b692cb5ff0258d0c575644826d
ndgigliotti <ndgigliotti@gmail.com>
2021-04-14 09:31:19 -0400

    add phase1 modules

---
+++ cleaning.py
@@ -0,0 +1,80 @@
+import re
+import json
+import unidecode
+from string import punctuation
+import pandas as pd
+import numpy as np
+import utils
+
+RE_PUNCT = re.compile(f"[{re.escape(punctuation)}]")
+RE_WHITESPACE = re.compile(r"\s+")
+
+
+def nan_info(data: pd.DataFrame):
+    df = data.isna().sum().to_frame("Total")
+    df["Percent"] = (df["Total"] / data.shape[0]) * 100
+    return df.sort_values("Total", ascending=False)
+
+
+def dup_info(data: pd.DataFrame):
+    df = data.duplicated().sum().to_frame("Total")
+    df["Percent"] = (df["Total"] / data.shape[0]) * 100
+    return df.sort_values("Total", ascending=False)
+
+
+def nan_rows(data: pd.DataFrame):
+    return data[data.isna().any(axis=1)]
+
+
+def who_is_nan(data: pd.DataFrame, col: str, name_col: str):
+    return nan_rows(data)[data[col].isna()][name_col]
+
+
+def process_strings(strings: pd.Series) -> pd.Series:
+    df = strings.str.lower()
+    df = df.str.replace(RE_PUNCT, "").str.replace(RE_WHITESPACE, " ")
+    df = df.map(unidecode.unidecode, na_action="ignore")
+    return df
+
+
+def detect_json_list(x):
+    return isinstance(x, str) and bool(re.fullmatch(r"\[.*\]", x))
+
+
+def coerce_list_likes(data):
+    if not isinstance(data, pd.Series):
+        raise TypeError("`data` must be pd.Series")
+    json_strs = data.map(detect_json_list, na_action="ignore")
+    clean = data.copy()
+    clean[json_strs] = clean.loc[json_strs].map(json.loads)
+    list_like = clean.map(pd.api.types.is_list_like)
+    clean[~list_like] = clean.loc[~list_like].map(lambda x: [x], na_action="ignore")
+    clean = clean.map(list, na_action="ignore")
+    return clean
+
+
+def find_outliers(data: pd.Series) -> pd.Series:
+    q1 = data.quantile(0.25, interpolation="midpoint")
+    q3 = data.quantile(0.75, interpolation="midpoint")
+    iqr = q3 - q1
+    min_cut = q1 - 1.5 * iqr
+    max_cut = q3 + 1.5 * iqr
+    return (data < min_cut) | (data > max_cut)
+
+
+def clip_outliers(data: pd.Series):
+    outliers = find_outliers(data)
+    inliers = data.loc[~outliers]
+    bounds = inliers.agg(["min", "max"])
+    return data.clip(lower=bounds["min"], upper=bounds["max"])
+
+
+def info(data: pd.DataFrame) -> pd.DataFrame:
+    n_rows = data.shape[0]
+    nan = data.isna().sum().to_frame("nan")
+    dup = data.apply(lambda x: x.duplicated()).sum().to_frame("dup")
+    info = pd.concat([nan, dup], axis=1)
+    pcts = (info / n_rows) * 100
+    pcts.columns = pcts.columns.map(lambda x: f"{x}_%")
+    info = pd.concat([info, pcts], axis=1)
+    return info.sort_index(axis=1).sort_values("nan", ascending=False)
---
+++ utils.py
@@ -0,0 +1,35 @@
+from collections.abc import Mapping
+import numpy as np
+import pandas as pd
+
+NULL = frozenset([np.nan, pd.NA, None])
+
+
+def numeric_cols(data: pd.DataFrame) -> list:
+    """Returns a list of all numeric column names.
+
+    Args:
+        data (pd.DataFrame): DataFrame to get column names from.
+
+    Returns:
+        list: All numeric column names.
+    """
+    numeric = data.dtypes.map(pd.api.types.is_numeric_dtype)
+    return data.columns[numeric].to_list()
+
+
+def map_list_likes(data: pd.Series, mapper: dict):
+    """Apply `mapper` to elements of elements of `data`.
+
+    Args:
+        data (pd.Series): Series containing only list-like elements.
+        mapper (dict): Dict-like or callable to apply to elements of elements of `data`.
+    """
+
+    def transform(list_):
+        if isinstance(mapper, Mapping):
+            return [mapper[x] if x not in NULL else x for x in list_]
+        else:
+            return [mapper(x) if x not in NULL else x for x in list_]
+
+    return data.map(transform, na_action="ignore")
